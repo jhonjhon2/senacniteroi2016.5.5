2ce4e38d3bba22dffa6513d53411dac26df25aaa
51115152016.5 <51115152016.5@51DE194284.senacrjedu.local>
2017-09-11 15:21:29 -0300

    Prova

---
+++ src/Respostas/Resposta5.java
@@ -0,0 +1,20 @@
+package Respostas;
+
+public class Resposta5 {
+	public static void main(String[] args) {
+	
+			
+		double valor1;
+		double valor2;
+		double valor3;
+		
+		
+		 println ("Insira o primeiro valor do Triângulo");
+		  
+		 
+		
+		
+	}
+
+
+}
